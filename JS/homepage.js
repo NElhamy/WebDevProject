bd01e1c3262c1416ba7c553e30e5a7bddc0ee68d
Ramez Ehab <ramezehab2@gmail.com>
2023-03-08 16:46:45 +0200

    added searchbox & improved responsiveness

--- JS/homepage.js
+++ JS/homepage.js
@@ -21,3 +21,42 @@ function makeSticky() {
         topNav.classList.remove("sticky");
     }
 }
+
+let menuIcons = document.getElementsByClassName("menuIcon");
+let searchImg = document.getElementById("searchImg");
+let searchSpan = document.getElementsByClassName("searchSpan")[0];
+let searchBox = document.getElementsByClassName("searchBox")[0];
+let closeSearch = document.getElementById("closeSearch");
+
+function toggleSearchBar(bool) {
+    let value;
+    if (bool) {
+        if (searchSpan.style.display == "inline") 
+            return;
+        value = "none";
+        searchSpan.style.marginLeft= "0";
+        searchSpan.style.display = "inline";
+        searchImg.style.marginLeft="0";
+        searchBox.style.width = "100%";
+        searchSpan.style.width = "70%";
+    }
+    else {
+        value = "";
+        searchSpan.style.marginLeft= "auto";
+        searchSpan.style.display = "none";
+        searchImg.style.marginLeft="auto";
+        searchBox.style.width = "unset";
+        searchSpan.style.width = "unset";
+    }
+
+    for (let i = 0;i<menuIcons.length;i++) {
+        menuIcons[i].style.display = value;
+    }
+
+    if (bool)
+        closeSearch.style.display = "";
+    else 
+        closeSearch.style.display = "none";
+
+    searchImg.style.display= "";
+}
